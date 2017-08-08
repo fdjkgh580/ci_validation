171aea0bd0f464fd906fe554aa99bbee7f65f73c
fdjkgh580 <fdjkgh580@yahoo.com.tw>
2017-08-08 16:49:05 +0800

    +

---
+++ src/Valication.php
@@ -0,0 +1,85 @@
+<?php 
+namespace Jsnlib;
+/**
+ * 驗證 API 輸入的參數
+ */
+class Validation 
+{
+    /**
+     * 沒有填寫參數？
+     */
+    static public function no_input($data, array $options = []): bool
+    {
+        return self::input($data, $options) === false;
+    }
+
+    /**
+     * 有填寫參數？
+     * TRUE             O
+     * FALSE            O
+     * NULL             X
+     * (string)''       X
+     * (int)0           O
+     * (float)0.5       O
+     * (int)-1          O
+     * (int)1           O
+     * (string)'1'      O
+     * (string)'0'      O
+     * (object) Class   O
+     * @param  $data    判斷的值
+     * @param  array    $options  擴充的額外判斷  
+     */
+    static public function input($data, array $options = []): bool
+    {
+        if ($options['string_empty'] === true)
+        {
+            return 
+            (
+                !is_null($data)
+            ) 
+                ? true
+                : false;
+        }
+
+        return 
+        (
+            !is_null($data) and
+            $data !== ""
+        ) 
+            ? true
+            : false;
+    }
+
+    
+}
+
+/*
+
+echo "<table border=1>";
+
+foreach ([true, false, null, '', 0, 0.5, -1, 1, "0", "1", new \Jsnlib\Ao] as $a)
+{
+    echo "<tr>";
+        echo "<td>";
+            var_dump($a);
+        echo "</td>";
+        echo "<td>";
+            echo gettype($a);
+        echo "</td>";
+        echo "<td>";
+            if (\Lib\Jsnlib\Validation::input($a, ['string_empty' => false])) 
+            {
+                echo "有帶入參數";
+            }
+        echo "</td>";
+        echo "<td>";
+            if (\Lib\Jsnlib\Validation::no_input($a, ['string_empty' => false])) 
+            {
+                echo "沒有參數";
+            }
+        echo "</td>";
+    echo "</tr>";
+}
+echo "</table>";
+
+ */
